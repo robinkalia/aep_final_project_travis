486293d357dc1df45edec9636d8d83be2f7c59e5
Robin Kalia <robinkalia@berkeley.edu>
2015-05-01 02:22:27 -0700

    BUILD 2: 2 Unit tests for Choice Methods Test

--- survey/tests.py
+++ survey/tests.py
@@ -17,10 +17,19 @@ class ChoiceMethodsTest(TestCase):
 
 	def test_contains_nonempty_text_with_empty_text(self):
 		choice = Choice(choice_text = '')
-		self.assertFalse(choice.contains_nonempty_text())			
- 
+		self.assertFalse(choice.contains_nonempty_text())			 
 	def test_contains_nonempty_text_with_valid_text(self):
 		choice = Choice(choice_text = 'San Francisco 49ers')
 		self.assertTrue(choice.contains_nonempty_text())
  
+	def test_contains_verylong_text_with_normal_size_text(self):
+		choice = Choice(choice_text = 'Tarantula')
+		self.assertFalse(choice.contains_verylong_text())
+ 
+	def test_contains_verylong_text_with_big_size_text(self):
+		text_str = "London"
+		choice = Choice(choice_text =  "{:201}".format(text_str))
+		self.assertTrue(choice.contains_verylong_text())
+
+
 
